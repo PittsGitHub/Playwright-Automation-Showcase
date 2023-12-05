c1ec244b5c74e8cc463f24ffca98eb93aae0f581
Daniel Pittard <126673498+PittsGitHub@users.noreply.github.com>
2023-12-05 16:08:39 +0000

    eslint update and config fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   root: true,
   plugins: ['@typescript-eslint', 'import', 'prettier'],
-  extends: ['airbnb-typescript/base', 'prettier', 'plugin:@typescript-eslint/recommended', 'plugin:import/typescript'],
+  extends: ['prettier', 'plugin:@typescript-eslint/recommended', 'plugin:import/typescript'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
