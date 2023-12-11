79a8367b86d6e824ff1c55f140678a3e48df44fd
Daniel Pittard <126673498+PittsGitHub@users.noreply.github.com>
2023-12-11 17:14:40 +0000

    Playwright ESlint Update No warnings on tests with no expect

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-floating-promises': 'error',
     '@typescript-eslint/no-misused-promises': 'error',
+    'playwright/expect-expect': 'off',
+    'playwright/no-skipped-test': 'off',
   },
   root: true,
   plugins: ['@typescript-eslint', 'import', 'prettier'],
